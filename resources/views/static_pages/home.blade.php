062c2cefc932e79cab108029644362d83ba2cb1c
2284624967 <2284624967@qq.com>
2021-02-19 02:44:03 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">你现在看到的是一个<a href="https://learnku.com/courses/laravel-essential-training">Laravel教程</a>项目示例</p>
+        <p>一起从这里开始</p>
+        <p><a class="btn btn-success btn-lg" href="#" role="button">现在注册</a></p>
+    </div>
 @stop
