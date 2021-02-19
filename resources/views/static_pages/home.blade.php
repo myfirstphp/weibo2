9ef41f9b855c850474479e5c8c8d65bbf79ff56e
2284624967 <2284624967@qq.com>
2021-02-19 09:14:31 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,6 @@
         <h1>Hello Laravel</h1>
         <p class="lead">你现在看到的是一个<a href="https://learnku.com/courses/laravel-essential-training">Laravel教程</a>项目示例</p>
         <p>一起从这里开始</p>
-        <p><a class="btn btn-success btn-lg" href="#" role="button">现在注册</a></p>
+        <p><a class="btn btn-success btn-lg" href="{{ route('signup') }}" role="button" target="_blank">现在注册</a></p>
     </div>
 @stop
