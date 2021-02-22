889ebcbab3b16194c1fa5c3aad88f5dd5d7ee936
2284624967 <2284624967@qq.com>
2021-02-22 11:57:31 +0000

    用户显示页面

--- resources/views/static_pages/test.blade.php
+++ resources/views/static_pages/test.blade.php
@@ -1,13 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>菜鸟教程(runoob.com)</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-<a href="#">我</a>
-<a href="#">在</a>
-<img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png"img>
-</body>
-</html>
+<a href="{{route('users.show', $user->id)}}">
+    <img class="gravatar" src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}">
+</a>
+<h1>{{ $user->name }}</h1>
+
+
+
+publice function gravatar($size='100')
+{
+    $hash=md5(strtolower(trim($this->attributes['email'])));
+    return "#/$hash?$size"
+}
+
+@extends('layouts.default')
+@section('title', $user->name)
+
+@section('content')
+<div class="row">
+    <div class="offset-md-2 col-md-8">
+        <div class="col-md-12">
+            <div class="offset-md-2 col-md-8">
+                <section class="user_info">
+                    @include('shared._user_info', ['user'=>$user])
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
