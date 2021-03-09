057ed3b293304f41edcd95e47fa0935f164c51d9
2284624967 <2284624967@qq.com>
2021-03-09 11:11:17 +0000

    repair

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -114,8 +114,8 @@ class UsersController extends Controller
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($messag) use ($from, $name, $to, $subject) {
-            $messag->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
         });
     }
 }
