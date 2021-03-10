e43938675dbe1c603b0978b1c8a2c45ca008b323
2284624967 <2284624967@qq.com>
2021-03-10 09:27:56 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,10 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    //一对多
+    public function statuses()
+    {
+        return $this->hasMany('App\Models\Status');
+    }
 }
